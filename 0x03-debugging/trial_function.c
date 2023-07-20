9fd0cc1b366ed512526f54c047cc3aa67e5c708f
danieldibango <danieldibangoowusumanu@gmail.com>
2023-07-20 14:26:46 -0700

    commiting

---
+++ 0x03-debugging/trial_function.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+*i is the variable to print
+*testing a while loop
+*/
+
+int main(void)
+{
+        int i;
+        i = 6;
+
+        while(i--)
+        {
+                printf("%d", i);
+        }
+	printf("\n"); /* add a newline character to separate the output from the shell prompt*/
+	return 0;
+}
