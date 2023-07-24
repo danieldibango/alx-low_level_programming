911bc0d32863601c6b3a8dd9bc1013c80798e3bb
danieldibango <danieldibangoowusumanu@gmail.com>
2023-07-24 14:28:18 -0700

    reset function for a pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - this is a function that resets the value of a memory space
+ * @n: is the pointer to the memory space
+ * Return: n
+ */
+
+int reset_to_98(int *n)
+{
+	*n = 98;
+}
